198445dfc2a430da549b9dbc2958b1987ac6d67f
sudoajay <sudoajay@gmail.com>
2018-09-30 09:55:10 +0530

    add new class

--- app/src/main/java/to_do_list/com/sudoajay/MainActivity.java
+++ app/src/main/java/to_do_list/com/sudoajay/MainActivity.java
@@ -1,13 +1,34 @@
 package to_do_list.com.sudoajay;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    // global variable
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Refrence here
+        Refernece();
+    }
+
+    public void Refernece(){
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // use Intent to pass on Create new Activity
+                Intent intent = new Intent(getApplicationContext(),Create_New_To_Do_Lsit.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
