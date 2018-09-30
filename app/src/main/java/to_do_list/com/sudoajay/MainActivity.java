2554468fb8ea99d3505cd8bdeae2c6dbe0eda4c5
sudoajay <sudoajay@gmail.com>
2018-09-30 07:51:23 +0530

    First commit

---
+++ app/src/main/java/to_do_list/com/sudoajay/MainActivity.java
@@ -0,0 +1,13 @@
+package to_do_list.com.sudoajay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
